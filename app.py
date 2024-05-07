8e5f8e6ba6767cdef1d50b4ffbf507b8eaf8475e
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-05-06 23:51:57 -0300

    edit

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 import requests
+import os
+
 
 app = Flask(__name__)
 
@@ -12,5 +14,6 @@ def get_instagram_user_info(username):
     else:
         return jsonify({"error": "Failed to fetch data"}), 404
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
