9526589dcde462e24cc41fcfadf680f57a7f9c88
Lucas Henrique Lopes Costa <lucass20586@gmail.com>
2024-05-13 07:40:25 -0300

    edit

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/api/instagram/<username>')
+
+@app.route("/api/instagram/<username>")
 def get_instagram_user_info(username):
     url = f"https://storiesig.info/api/ig/userInfoByUsername/{username}"
     response = requests.get(url)
@@ -13,6 +14,7 @@ def get_instagram_user_info(username):
         return jsonify(response.json())
     else:
         return jsonify({"error": "Failed to fetch data"}), 404
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
